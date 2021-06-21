6f1d6bb53ceefd7c7259f53a6e3f0e03b58b7807
Carl Stimson <carlstimson@gmail.com>
2021-06-21 12:42:48 +0900

    Adds select_nearest_songs function, adds model directory
    Adds the select_nearest_songs function from the notebook, along with the necessary model files. Also modifies the predict HTML to show more info on the predicted track (only a base case at the moment)

--- flask_app/spotify_dashboard.py
+++ flask_app/spotify_dashboard.py
@@ -1,6 +1,7 @@
 """Spotify dashboard with Flask."""
 from flask import Flask, request, render_template, url_for, redirect
 from flask_app.modules import get_track, radar_plot
+from flask_app.predict import recommendation
 
 
 APP = Flask(__name__)
@@ -19,14 +20,25 @@ def index():
 
 @APP.route('/recommend', methods=['GET', 'POST'])
 def recommend():
-    """Asks user to input track to analyze"""
+    """Asks user to input track to get recommendations"""
+    if request.method == 'POST':
+        track = request.form['track']
+        # Using specific ID. Replace with input from model.
+        recommended = recommendation('0cELvuwJW1acISUHYB6suj')
+        return render_template('prediction.html', track=track, recommended=recommended)
+    return render_template('recommend.html')
+
+
+@APP.route('/prediction', methods=['GET', 'POST'])
+def prediction():
+    """Asks user to input track to get recommendations"""
     if request.method == 'POST':
         track = request.form['track']
         # Get model prediction for track
         # model = ...
         pred = 'Happy Birthday!'
         return render_template('prediction.html', track=track, pred=pred)
-    return render_template('recommend.html')
+    return render_template('prediction.html')
 
 
 @APP.route('/')
