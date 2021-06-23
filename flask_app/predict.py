8b7de0d9ae2a920629a7ddc9cf78b059ef6e954e
Carl Stimson <carlstimson@gmail.com>
2021-06-23 11:56:18 +0900

    Functional browser API
    Includes a page to enter track ID for audio feature visualization, and a page to enter ID for song recommendations.

--- flask_app/predict.py
+++ flask_app/predict.py
@@ -1,59 +1,74 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from flask_app.modules import get_track
-from joblib import load
+import joblib
 import pandas as pd
 import pickle
 
-# import numpy as np
-# from sklearn.pipeline import Pipeline
-# from sklearn.feature_extraction.text import TfidfVectorizer
-# from re import compile as rcompile
-# from sklearn.model_selection import train_test_split
-# from sklearn.neighbors import NearestNeighbors
-
 # Load model
-nn = load('../model/nn.joblib')
-dtm_filename = '../model/nlp_dtm.pkl'
+# path = '../model/nn.joblib'
+path = '/Users/Carl/Desktop/datascience/spotify_song_suggester/model/nn.joblib'
+nn = joblib.load(path)
+# dtm_filename = '../model/nlp_dtm.pkl'
+dtm_filename = '/Users/Carl/Desktop/datascience/spotify_song_suggester/model/nlp_dtm.pkl'
 dtm = pickle.load(open(dtm_filename, 'rb'))
 
 # Connect to spotify API
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
+# Load song dataframe
+# path = '../data/spotify_songs.csv'
+path = '/Users/Carl/Desktop/datascience/spotify_song_suggester/data/spotify_songs.csv'
+df = pd.read_csv(path)
 
-def recommendation(track_id):
+
+def recommendation(track_list):
     """Takes track_id from model, returns data on the track"""
     # Get track data
-    track = spotify.track('spotify:track:' + track_id)
-    track_url = 'https://open.spotify.com/track/' + track_id
-    title = track['album']['name']
-    artist = track['album']['artists'][0]['name']
-    album_cover = track['album']['images'][1]['url']
-    # Put data in dictionary
-    recommended = {'url': track_url, 'artist': artist,
-                         'title': title, 'album_cover': album_cover}
+    recommended = []
+    for i in range(5):
+        track = spotify.track('spotify:track:' + track_list[i]['id'])
+        track_url = 'https://open.spotify.com/track/' + track_list[i]['id']
+        title = track['name']
+        artist = track['album']['artists'][0]['name']
+        album_cover = track['album']['images'][1]['url']
+        recommended.append({'url': track_url, 'artist': artist,
+                            'title': title, 'album_cover': album_cover})
+        pass
     return recommended
-
-# print(recommendation('Tigerlily'))
 
 
 def select_nearest_songs(id):
-    df = pd.read_csv('../data/spotify_songs.csv')
-
-    track_num = df.loc[df['track_id'] == id]
+    df2 = wrangle(df)
+    track_num = df2.loc[df2['track_id'] == id]
     x = track_num.index
     x = x[0]
     x = x.item()
     doc = dtm.loc[x].values
     result = nn.kneighbors([doc], n_neighbors=6)
-    rec_songs = {"id" :[]};
-
+    rec_songs = []
     for i in range(5):
-        song = result[1][0][ 1 +i]
+        song = result[1][0][1 + i]
         id = df.loc[song]['track_id']
-        rec_songs['id'].append(id)
+        rec_songs.append({'id': id})
     return rec_songs
 
 
-select_nearest_songs('00cqd6ZsSkLZqGMlQCR0Zo')
+def wrangle(data):
+    # drop null values
+    data = data.dropna()
+    # restrict to songs with lyrics in English
+    data = data[data['language'] == 'en']
+    # drop duplicates
+    data = data.drop_duplicates(subset=['track_name', 'track_artist'],
+                                keep='first')
+    # Reduce features in dataset
+    features = ['track_id', 'track_name', 'track_artist', 'lyrics', 'track_album_name', 'playlist_name',
+                'playlist_genre']
+    data = data[features]
+    # reset index
+    data = data.reset_index()
+    return data
 
+
+i = select_nearest_songs('6b9SBw4AxSJUYEetOS5Sc8')
+print(recommendation(i))
--- flask_app/spotify_dashboard.py
+++ flask_app/spotify_dashboard.py
@@ -1,8 +1,7 @@
 """Spotify dashboard with Flask."""
 from flask import Flask, request, render_template, url_for, redirect
-from flask_app.modules import get_track, radar_plot
-from flask_app.predict import recommendation
-
+from flask_app.modules import get_track, radar_plot, random_track, grab_five
+from flask_app.predict import recommendation, select_nearest_songs
 
 APP = Flask(__name__)
 
@@ -10,35 +9,34 @@ APP = Flask(__name__)
 @APP.route('/index', methods=['GET', 'POST'])
 def index():
     """Asks user to input track to analyze"""
+    random_sample = grab_five()
     if request.method == 'POST':
-        track = request.form['track']
-        track_features = get_track(track)
+        track_id = request.form['track_id']
+        # Get random id if user does not input
+        if not track_id:
+            track_id = random_track()
+        track_features = get_track(track_id)
         fig, html = radar_plot(track_features)
         return html, render_template('visualize.html', track_features=track_features)
-    return render_template('index.html')
+    return render_template('index.html', random_sample=random_sample)
 
 
 @APP.route('/recommend', methods=['GET', 'POST'])
 def recommend():
     """Asks user to input track to get recommendations"""
+    random_sample = grab_five()
     if request.method == 'POST':
-        track = request.form['track']
-        # Using specific ID. Replace with input from model.
-        recommended = recommendation('0cELvuwJW1acISUHYB6suj')
-        return render_template('prediction.html', track=track, recommended=recommended)
-    return render_template('recommend.html')
-
-
-@APP.route('/prediction', methods=['GET', 'POST'])
-def prediction():
-    """Asks user to input track to get recommendations"""
-    if request.method == 'POST':
-        track = request.form['track']
-        # Get model prediction for track
-        # model = ...
-        pred = 'Happy Birthday!'
-        return render_template('prediction.html', track=track, pred=pred)
-    return render_template('prediction.html')
+        track_id = request.form['track_id']
+        # Get random id if user does not input
+        if not track_id:
+            track_id = random_track()
+        input_track = get_track(track_id)
+        # Model results
+        rec_songs = select_nearest_songs(track_id)
+        output_tracks = recommendation(rec_songs)
+        return render_template('prediction.html',
+                               input_track=input_track, output_tracks=output_tracks)
+    return render_template('recommend.html', random_sample=random_sample)
 
 
 @APP.route('/')
